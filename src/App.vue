78d1f962c08938bdc0f75fd997db4c764374d58f
Igor Oliveira <m.igoroliveira@gmail.com>
2022-04-28 12:37:39 +0100

    Clients table (#5)

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
 	<main>
-		<CreateClient />
-		<h1>Hello from clients</h1>
-		<ul v-for="client in clients" :key="client.key" class="clients">
-			<li>{{ client.name }}</li>
-		</ul>
+		<ClientsTable :clients="clients" />
 	</main>
 </template>
 
 <script>
-import CreateClient from "./components/CreateClient";
+import ClientsTable from "./components/ClientsTable";
 
 export default {
 	name: "App",
 	components: {
-		CreateClient,
+		ClientsTable,
 	},
 	props: {
 		clients: {
@@ -28,7 +24,14 @@ export default {
 </script>
 
 <style scoped>
-.clients {
-	font-weight: bold;
+main {
+	width: 80%;
+	margin: auto;
+	min-height: 100vh;
+	padding: 20px;
+	display: flex;
+	gap: 20px;
+	flex-direction: column;
+	align-content: center;
 }
 </style>
