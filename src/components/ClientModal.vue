59e5313438dfd190903ce8a2b48b3a7f3f231538
Igor Oliveira <m.igoroliveira@gmail.com>
2022-04-29 12:21:07 +0100

    initial header

--- src/components/ClientModal.vue
+++ src/components/ClientModal.vue
@@ -2,7 +2,24 @@
 	<div>
 		<Modal @close="toggleModal()">
 			<div class="modal-content">
-				{{ client }}
+				<div class="row">
+					<Avatar :username="client.name" :size="100" />
+					<div class="col ml-22">
+						<h1>
+							{{ client.name }}
+						</h1>
+						<p>{{ client.timezone }}</p>
+						<p>{{ age }}</p>
+					</div>
+					<div class="col ml-22">
+						<h3>Other Contacts</h3>
+						<h3>Attachments</h3>
+					</div>
+				</div>
+				<h3>About</h3>
+				<p>
+					{{ client.description }}
+				</p>
 			</div>
 		</Modal>
 	</div>
@@ -10,9 +27,12 @@
 
 <script>
 import { Modal } from "@nextcloud/vue";
+import Avatar from "vue-avatar";
+
 export default {
 	components: {
 		Modal,
+		Avatar,
 	},
 
 	props: {
@@ -21,6 +41,11 @@ export default {
 			default() {
 				return {};
 			},
+		},
+	},
+	computed: {
+		age() {
+			return this.client.age + " Years Old";
 		},
 	},
 	methods: {
@@ -48,5 +73,37 @@ button {
 
 .modal-content {
 	padding: 20px;
+}
+
+.client-main {
+	display: flex;
+}
+
+.modal-content h1 {
+	font-weight: 500;
+	font-size: 33px;
+	line-height: 41px;
+	color: #346188;
+}
+
+.modal-content h3 {
+	font-weight: 600;
+	font-size: 18px;
+	line-height: 22px;
+	color: #346188;
+}
+
+.col {
+	display: flex;
+	flex-direction: column;
+}
+
+.row {
+	display: flex;
+	flex-direction: row;
+}
+
+.ml-22 {
+	margin-left: 22px;
 }
 </style>
