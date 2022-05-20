20e62095f8072c2f0f450bd100f167173d4d9e0a
Martin Pirate <sirmartins6@gmail.com>
2022-05-20 13:48:09 +0300

    Add overview cards to Clients main view (#8)
    Co-authored-by: Martinskiragu <m.kiragu@sweetnexus.co.uk>

---
+++ src/components/Overview.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="card-overview">
+		<CardOverview :show-data="true">
+			<template #title> Total Earnings</template>
+		</CardOverview>
+		<CardOverview :show-data="true">
+			<template #title> Sessions</template>
+		</CardOverview>
+		<CardOverview :show-data="true">
+			<template #title>Total Hours</template>
+		</CardOverview>
+		<CardOverview :show-data="true">
+			<template #title>Total Hours</template>
+		</CardOverview>
+	</div>
+</template>
+
+<script>
+import CardOverview from "./CardOverview";
+
+export default {
+	name: "Overview",
+	components: {
+		CardOverview,
+	},
+};
+</script>
+
+<style scoped>
+.card-overview {
+	display: flex;
+	width: 80%;
+	margin: auto;
+	padding: 20px;
+}
+</style>
